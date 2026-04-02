23de8df74e6ea09508b7fcc9f7c2f5d507a5f856
sparkkingv <vikashtech1106@gmail.com>
2026-04-02 18:11:41 +0530

    add brute force module

--- brute_force.py
+++ brute_force.py
@@ -1,8 +1,13 @@
+import time
+
 crt_uname = "admin"
 crt_pass = "admin"
 
 attempts = 0
- 
+delay = 0.1
+
+print("=== CYSEC BRUTE FORCE TOOL ===\n")
+
 with open ("adm_list.txt","r") as admlist:
     for uname in admlist:
          usrname = uname.strip()
@@ -11,16 +16,15 @@ with open ("adm_list.txt","r") as admlist:
             for line in file:
                 password = line.strip()
                 attempts += 1
-                import time
-                time.sleep(0.1)
+                time.sleep(delay)
 
-                print(f"\nTrying : {usrname} : {password}")
+                print(f"\n [{attempts}] Trying -> {usrname} : {password}")
 
                 if password == crt_pass and usrname == crt_uname:
                     print(f"\n[+] password found! \n")
                     print(f"Attempts : {attempts}")
                     print(f"username : {usrname}")
                     print(f"password : {password}\n")
-                    break
-                else:
-                    print(f"password not found!")
+                    exit()
+
+print(f"\nAttack Failed! credientials not found\n ")
